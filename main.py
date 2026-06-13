91275b6474108125a8c3f393675ac3b3767a6e8f
Robin2883 <bluebillu83@gmail.com>
2026-06-14 00:40:39 +0600

    vector db client integrated. Inngest server integrated with the workflow

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ import inngest
 import inngest.fast_api
 from inngest.experimental import ai
 from dotenv import load_dotenv
+import uuid
+import pydantic
 import os
 import uvicorn
 import datetime
 
 from data_load import load_chunk_pdf, embed_texts
 from vector_db import QdrantStorage
+from custom import RagChunkSrc, RAGQueryResult, RAGSearchResult, RAGUpsertResult
 
 load_dotenv()
 
@@ -21,8 +24,24 @@ inngest_client=inngest.Inngest(app_id='rag_app', logger=logging.getLogger("uvico
 )
 
 async def ingest_pdf(ctx: inngest.Context) -> str:
-    ctx.logger.info(ctx.event)
-    return "done"
+    def _load(ctx: inngest.Context) ->RagChunkSrc:
+        pdf_path=ctx.event.data["pdf_path"]
+        source_id=ctx.event.data.get("source_id", pdf_path)
+        chunks=load_chunk_pdf(pdf_path)
+        return RagChunkSrc(chunks=chunks, source_id=source_id)
+    
+    def _upsert(chunksandsrc: RagChunkSrc) ->RAGUpsertResult:
+        chunks=chunksandsrc.chunks
+        source_id=chunksandsrc.source_id
+        vecs=embed_texts(chunks)
+        ids=[str(uuid.uuid5(uuid.NAMESPACE_URL, f"{source_id}:{i}")) for i in range(len(chunks))]
+        payloads=[{"source":source_id, "text": chunks[i]} for i in range(len(chunks))]
+        QdrantStorage().upsert(ids, vecs, payloads)
+        return RAGUpsertResult(ingested=len(chunks))
+
+    chunksandsrc=await ctx.step.run("Load-chunk", lambda: _load(ctx), output_type=RagChunkSrc)
+    ingested=await ctx.step.run ("embed-upsert", lambda: _upsert(chunksandsrc), output_type=RAGUpsertResult)
+    return ingested.model_dump()
 
 app=FastAPI()
 
--- vector_db.py
+++ vector_db.py
@@ -2,11 +2,12 @@ from qdrant_client import QdrantClient
 from qdrant_client.models import Distance, VectorParams, PointStruct
 
 class QdrantStorage:
-    def __init__(self, url="http://localhost:6333", collection="docs", dim=3072):
+    def __init__(self, url="http://localhost:6333", collection="docs", dim=384):
         self.client=QdrantClient(url=url, timeout=30)
         self.collection=collection
-        if not self.client.collection_exists(self.collection):
-            self.client.create_collection(collection_name=self.collection, vectors_config=VectorParams(size=dim, distance=Distance.COSINE))
+        if self.client.collection_exists(self.collection):
+            self.client.delete_collection(self.collection)
+        self.client.create_collection(collection_name=self.collection, vectors_config=VectorParams(size=dim, distance=Distance.COSINE))
         self.dim=dim    
 
     def upsert(self, ids, vectors, payloads):
